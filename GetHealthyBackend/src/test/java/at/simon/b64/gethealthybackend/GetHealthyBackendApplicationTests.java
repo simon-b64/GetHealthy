ea92aefc285c24a9953b230ba3d3134ae3108a4a
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-02-18 18:44:53 +0100

    fix: Fixed Test failures by adding test profile

--- GetHealthyBackend/src/test/java/at/simon/b64/gethealthybackend/GetHealthyBackendApplicationTests.java
+++ GetHealthyBackend/src/test/java/at/simon/b64/gethealthybackend/GetHealthyBackendApplicationTests.java
@@ -2,8 +2,10 @@ package at.simon.b64.gethealthybackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GetHealthyBackendApplicationTests {
 
     @Test
