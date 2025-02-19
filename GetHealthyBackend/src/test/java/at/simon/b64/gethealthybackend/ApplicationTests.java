c502955ae67acdecdc5855397691f4a23fdbb9f4
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-02-19 19:22:37 +0100

    chore: Renamed GetHealthyBackendApplication to Application

--- GetHealthyBackend/src/test/java/at/simon/b64/gethealthybackend/ApplicationTests.java
+++ GetHealthyBackend/src/test/java/at/simon/b64/gethealthybackend/ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class GetHealthyBackendApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
